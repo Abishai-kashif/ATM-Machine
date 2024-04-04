7a130a7b8ca0b8d98a0dc33f0f833ebdd275756b
Abishai-kashif <abishaikashif@gmail.com>
2024-04-04 19:04:57 +0500

    bug fixed

--- index.js
+++ index.js
@@ -1,99 +1,138 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 import chalk from "chalk";
+// initializing user pin and current balnce
 let pin = 1234;
 let currentBalance = 10000;
-let depositLimit = 200000; //limit for deposit at a time
+//limit for deposit at a time
+let depositLimit = 200000;
 //welcome message
-console.log((`\n\t\t\t\t  ==========================`));
-console.log(`\t\t<==={==={==={==={ ${chalk.blueBright("WELCOME TO MY ATM MACHINE!")} }===}===}===}===>`);
-console.log((`\t\t\t\t  ==========================\n\n`));
+console.log((`\n\t\t\t\t   <<========================>>`));
+console.log(`\t\t<<==={==={==={==={  ${chalk.blueBright("WELCOME TO MY ATM MACHINE!")}  }===}===}===}===>>`);
+console.log((`\t\t\t\t   <<========================>>\n\n`));
 // asking user to enter the pin
 let pinAnswer = await inquirer.prompt({
     name: "pin",
     type: "password",
     message: "Kindly enter your pin : "
 });
-console.log(""); // leaving some empty lines
 //checks if pin is correct
 if (Number(pinAnswer.pin) === pin) {
+    console.log(chalk.greenBright("Your pin is correct login successfully\n"));
     //asking user to select operation
-    let operations = await inquirer.prompt({
+    let selectedOperation = await inquirer.prompt({
         name: "operation",
         type: "list",
         message: "select the operation :",
-        choices: ["Deposit Cash", "Withdraw Cash",
-            "Fast Cash", "Current Balance"]
+        choices: ["Deposit Cash", "Withdraw Cash", "Current Balance"]
     });
     console.log(""); // leaving some empty lines
     // if selected operation is deposit
-    if (operations.operation === "Deposit Cash") {
+    if (selectedOperation.operation === "Deposit Cash") {
         let deposit = await inquirer.prompt({
             name: "amount1",
             type: "number",
             message: "Enter amount : "
         });
-        currentBalance += deposit.amount1;
         //checks if amount is less than deposit limit
         if (deposit.amount1 <= depositLimit) {
-            console.log(chalk.greenBright(`\n-> Congrats! You have deposited : ${deposit.amount1}$.`));
-            console.log(chalk.white.bold(`-> Your current balance is : ${currentBalance}$.`));
+            currentBalance += deposit.amount1;
+            console.log(chalk.white.bold(`\n-> Congrats! You have deposited : ${chalk.greenBright(`${deposit.amount1}$`)}.`));
+            if (currentBalance === 0) {
+                console.log(chalk.white.bold(`-> Your current balance is ${chalk.red(`${currentBalance}$`)}.`));
+            }
+            else {
+                console.log(chalk.white.bold(`-> Your current balance is : ${chalk.greenBright(`${currentBalance}$`)}.`));
+            }
+            ;
         }
         else {
-            console.log(chalk.red(`The maximum deposit limit is : ${depositLimit}$.`));
+            console.log(chalk.red(`You exceeded your maximum deposit limit : ${depositLimit}$`));
         }
         ;
     }
     ;
     // if selected operation is withdrawal
-    if (operations.operation === "Withdraw Cash") {
-        let deduct = await inquirer.prompt({
-            name: "amount",
-            type: "number",
-            message: "Enter amount : "
+    if (selectedOperation.operation === "Withdraw Cash") {
+        //ask for method of withdrawal
+        let selectedMethod = await inquirer.prompt({
+            name: "method",
+            type: "list",
+            message: "Choose withdrawal method :",
+            choices: ["Fast Cash", "Enter Amount"]
         });
-        //checks if amount is less than current balance
-        if (deduct.amount <= currentBalance) {
-            currentBalance -= deduct.amount;
-            console.log(chalk.greenBright(`\n-> Congrats! You have withdrawn : ${deduct.amount}$.`));
-            console.log(chalk.white.bold(`-> The remaining balance is : ${currentBalance}$.`));
-        }
-        else {
-            console.log(chalk.red("Your current balance is insufficient for this withdrawal."));
+        console.log(""); // leaving some empty lines
+        // if selected method is fast cash
+        if (selectedMethod.method === "Fast Cash") {
+            //give some amount options
+            let selectedAmounts = await inquirer.prompt({
+                name: "selectedAmount",
+                type: "list",
+                message: "Select the amount :",
+                choices: ["500", "1000", "5000", "10000"]
+            });
+            if (selectedAmounts.selectedAmount <= currentBalance) {
+                currentBalance -= selectedAmounts.selectedAmount;
+                console.log(chalk.white.bold(`\n-> Congrats! You have withdrawn : ${chalk.greenBright(`${selectedAmounts.selectedAmount}$`)}.`));
+                //checks if balance is zero
+                if (currentBalance === 0) {
+                    console.log(chalk.white.bold(`-> Your remaining balance is ${chalk.red(`${currentBalance}$`)}.`));
+                }
+                else {
+                    console.log(chalk.white.bold(`-> The remaining balance is : ${chalk.greenBright(`${currentBalance}$`)}.`));
+                }
+                ;
+            }
+            else {
+                console.log(chalk.red("Your current balance is insufficient for this withdrawal."));
+            }
+            ;
         }
         ;
-    }
-    ;
-    //if selected operation is fast cash
-    if (operations.operation === "Fast Cash") {
-        //give some amount options
-        let selectedAmounts = await inquirer.prompt({
-            name: "selectedAmount",
-            type: "list",
-            message: "Select the amount :",
-            choices: ["500", "1000", "5000", "10000"]
-        });
-        console.log(chalk.greenBright(`\n-> Congrats! You have withdrawn : ${selectedAmounts.selectedAmount}$.`));
-        console.log(chalk.white.bold(`-> The remaining balance is : ${currentBalance}$.`));
+        // if selected method is enter amount
+        if (selectedMethod.method === "Enter Amount") {
+            let deduct = await inquirer.prompt({
+                name: "amount",
+                type: "number",
+                message: "Enter amount : "
+            });
+            //checks if amount is less than current balance
+            if (deduct.amount <= currentBalance) {
+                currentBalance -= deduct.amount;
+                console.log(chalk.white.bold(`\n-> Congrats! You have withdrawn : ${chalk.greenBright(`${deduct.amount}$`)}.`));
+                if (currentBalance === 0) {
+                    console.log(chalk.white.bold(`-> Your remaining balance is ${chalk.red(`${currentBalance}$`)}.`));
+                }
+                else {
+                    console.log(chalk.white.bold(`-> Your remaining balance is : ${chalk.greenBright(`${currentBalance}$`)}.`));
+                }
+                ;
+            }
+            else {
+                console.log(chalk.red("Your current balance is insufficient for this withdrawal."));
+            }
+            ;
+        }
+        ;
     }
     ;
     // if selected operation is current balance
-    if (operations.operation === "Current Balance") {
+    if (selectedOperation.operation === "Current Balance") {
         if (currentBalance === 0) {
-            console.log(chalk.red("-> Your current balance is 0$."));
+            console.log(chalk.white.bold(`-> Your current balance is ${chalk.red(`${currentBalance}$`)}.`));
         }
         else {
-            console.log(chalk.greenBright(`-> Your current balance is : ${currentBalance}$.`));
+            console.log(chalk.white.bold(`-> Your current balance is : ${chalk.greenBright(`${currentBalance}$`)}.`));
         }
         ;
     }
     ;
+    //thank you message
+    console.log((`\n\n\t\t\t\t   =======================`));
+    console.log(`\t\t\t\t   ${chalk.blueBright("Thank you for visiting!")}`);
+    console.log((`\t\t\t\t   =======================\n`));
 }
 else {
-    console.log(chalk.red("-> Invalid pin. Please try again."));
+    console.log(chalk.red("-> Invalid pin."));
 }
 ;
-//thank you message
-console.log((`\n\n\t\t\t\t   =======================`));
-console.log(`\t\t\t\t   ${chalk.blueBright("Thank you for visiting!")}`);
-console.log((`\t\t\t\t   =======================\n`));
--- index.ts
+++ index.ts
@@ -4,16 +4,17 @@ import inquirer from "inquirer";
 import chalk from "chalk";
 
 
+// initializing user pin and current balnce
 let pin: number = 1234;
 let currentBalance: number = 10000;
 
-let depositLimit: number = 200000; //limit for deposit at a time
-
+//limit for deposit at a time
+let depositLimit: number = 200000;
 
 //welcome message
-console.log((`\n\t\t\t\t  ==========================`));
-console.log(`\t\t<==={==={==={==={ ${chalk.blueBright("WELCOME TO MY ATM MACHINE!")} }===}===}===}===>`);
-console.log((`\t\t\t\t  ==========================\n\n`));
+console.log((`\n\t\t\t\t   <<========================>>`));
+console.log(`\t\t<<==={==={==={==={  ${chalk.blueBright("WELCOME TO MY ATM MACHINE!")}  }===}===}===}===>>`);
+console.log((`\t\t\t\t   <<========================>>\n\n`));
 
 
 // asking user to enter the pin
@@ -25,21 +26,20 @@ let pinAnswer = await inquirer.prompt(
     }
 );
 
-console.log("");// leaving some empty lines
-
 
 //checks if pin is correct
 if (Number(pinAnswer.pin) === pin) {
 
+    console.log(chalk.greenBright("Your pin is correct login successfully\n"));
+    
 
     //asking user to select operation
-    let operations = await inquirer.prompt(
+    let selectedOperation = await inquirer.prompt(
         {
             name: "operation",
             type: "list",
             message: "select the operation :",
-            choices: ["Deposit Cash", "Withdraw Cash", 
-                        "Fast Cash" ,"Current Balance"]
+            choices: ["Deposit Cash", "Withdraw Cash", "Current Balance"]
         }
     );
 
@@ -47,7 +47,7 @@ if (Number(pinAnswer.pin) === pin) {
 
 
     // if selected operation is deposit
-    if (operations.operation === "Deposit Cash") {
+    if (selectedOperation.operation === "Deposit Cash") {
 
         let deposit = await inquirer.prompt(
             {
@@ -57,75 +57,118 @@ if (Number(pinAnswer.pin) === pin) {
             }
         );
 
-        currentBalance += deposit.amount1;
-
         //checks if amount is less than deposit limit
+        
         if (deposit.amount1 <= depositLimit) {
-            console.log(chalk.greenBright(`\n-> Congrats! You have deposited : ${deposit.amount1}$.`));
-            console.log(chalk.white.bold(`-> Your current balance is : ${currentBalance}$.`));
+
+                currentBalance += deposit.amount1;
+
+            console.log(chalk.white.bold(`\n-> Congrats! You have deposited : ${chalk.greenBright(`${deposit.amount1}$`)}.`));
+
+            if (currentBalance === 0) {
+                console.log(chalk.white.bold(`-> Your current balance is ${chalk.red(`${currentBalance}$`)}.`));
+            } else {
+                console.log(chalk.white.bold(`-> Your current balance is : ${chalk.greenBright(`${currentBalance}$`)}.`));
+            };
         } else {
-            console.log(chalk.red(`The maximum deposit limit is : ${depositLimit}$.`));
+            console.log(chalk.red(`You exceeded your maximum deposit limit : ${depositLimit}$`));
         };
     };
 
 
     // if selected operation is withdrawal
-    if (operations.operation === "Withdraw Cash") {
+    if (selectedOperation.operation === "Withdraw Cash") {
 
-        let deduct = await inquirer.prompt(
+        //ask for method of withdrawal
+        let selectedMethod = await inquirer.prompt(
             {
-                name: "amount",
-                type: "number",
-                message: "Enter amount : "
+                name: "method",
+                type: "list",
+                message: "Choose withdrawal method :",
+                choices: ["Fast Cash" ,"Enter Amount"]
             }
         );
 
-        //checks if amount is less than current balance
-        if (deduct.amount <= currentBalance) {
+        console.log("");// leaving some empty lines
 
-            currentBalance -= deduct.amount; 
+        // if selected method is fast cash
+        if (selectedMethod.method === "Fast Cash") {
 
-            console.log(chalk.greenBright(`\n-> Congrats! You have withdrawn : ${deduct.amount}$.`));
-            console.log(chalk.white.bold(`-> The remaining balance is : ${currentBalance}$.`));
-               
+
+                //give some amount options
+            let selectedAmounts = await inquirer.prompt(
+                {
+                    name: "selectedAmount",
+                    type: "list",
+                    message: "Select the amount :",
+                    choices: ["500" ,"1000" ,"5000" ,"10000"]
+                }
+            );
+
+        if (selectedAmounts.selectedAmount <= currentBalance) {
+
+            currentBalance -= selectedAmounts.selectedAmount; 
+
+            console.log(chalk.white.bold(`\n-> Congrats! You have withdrawn : ${chalk.greenBright(`${selectedAmounts.selectedAmount}$`)}.`));
+
+            //checks if balance is zero
+            if (currentBalance === 0) {
+                console.log(chalk.white.bold(`-> Your remaining balance is ${chalk.red(`${currentBalance}$`)}.`));
+            } else {
+                console.log(chalk.white.bold(`-> The remaining balance is : ${chalk.greenBright(`${currentBalance}$`)}.`));
+            };
+    
         } else {
             console.log(chalk.red("Your current balance is insufficient for this withdrawal."));
         };
     };
+        
 
-    //if selected operation is fast cash
-    if (operations.operation === "Fast Cash") {
+        // if selected method is enter amount
+        if (selectedMethod.method === "Enter Amount") {
 
-        //give some amount options
-        let selectedAmounts = await inquirer.prompt(
-            {
-                name: "selectedAmount",
-                type: "list",
-                message: "Select the amount :",
-                choices: ["500" ,"1000" ,"5000" ,"10000"]
-            }
-        );
+            let deduct = await inquirer.prompt(
+                {
+                    name: "amount",
+                    type: "number",
+                    message: "Enter amount : "
+                }
+            );
+            //checks if amount is less than current balance
+            if (deduct.amount <= currentBalance) {
 
-        console.log(chalk.greenBright(`\n-> Congrats! You have withdrawn : ${selectedAmounts.selectedAmount}$.`));
-        console.log(chalk.white.bold(`-> The remaining balance is : ${currentBalance}$.`));
+                currentBalance -= deduct.amount;
 
-    };
+                console.log(chalk.white.bold(`\n-> Congrats! You have withdrawn : ${chalk.greenBright(`${deduct.amount}$`)}.`));
 
-    // if selected operation is current balance
-    if (operations.operation === "Current Balance") {
-        if (currentBalance === 0) {
-            console.log(chalk.red("-> Your current balance is 0$."));
-
-        } else {
-            console.log(chalk.greenBright(`-> Your current balance is : ${currentBalance}$.`));
+                if (currentBalance === 0) {
+                console.log(chalk.white.bold(`-> Your remaining balance is ${chalk.red(`${currentBalance}$`)}.`));
+            } else {
+                console.log(chalk.white.bold(`-> Your remaining balance is : ${chalk.greenBright(`${currentBalance}$`)}.`));
+            };
+    
+            } else {
+                console.log(chalk.red("Your current balance is insufficient for this withdrawal."));
+            };
         };
     };
 
-} else {
-    console.log(chalk.red("-> Invalid pin. Please try again."));
-};
+    // if selected operation is current balance
+    if (selectedOperation.operation === "Current Balance") {
 
-//thank you message
-console.log((`\n\n\t\t\t\t   =======================`));
-console.log(`\t\t\t\t   ${chalk.blueBright("Thank you for visiting!")}`);
-console.log((`\t\t\t\t   =======================\n`));
+        if (currentBalance === 0) {
+            console.log(chalk.white.bold(`-> Your current balance is ${chalk.red(`${currentBalance}$`)}.`));
+
+        } else {
+            console.log(chalk.white.bold(`-> Your current balance is : ${chalk.greenBright(`${currentBalance}$`)}.`));
+        };
+    };
+
+    //thank you message
+    console.log((`\n\n\t\t\t\t   =======================`));
+    console.log(`\t\t\t\t   ${chalk.blueBright("Thank you for visiting!")}`);
+    console.log((`\t\t\t\t   =======================\n`));
+
+} else {
+    console.log(chalk.red("-> Invalid pin."));
+};
